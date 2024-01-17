d4ddc411e34c481f1080b17f43215558519d2177
Erbol Kasymbek <erbafredev@gmail.com>
2024-01-17 18:16:14 +0600

    changed click event to hello Everyone

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const title = document.querySelector("h1");
 const btnGreet = document.querySelector("button");
 
 title.addEventListener("click", () => {
-  title.textContent = "Hello John";
+  title.textContent = "Hello Everyone";
 });
 
 function greet(name) {
