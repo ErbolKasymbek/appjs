14ec62e73217206d4be16ce34ab52b9ad4be37ac
Erbol Kasymbek <erbafredev@gmail.com>
2024-01-20 02:41:32 +0600

    first modify

--- js/app.js
+++ js/app.js
@@ -53,3 +53,35 @@ btnReset.addEventListener("click", () => {
     });
   });
 });
+
+// const btnsGroup = [btnId, btnName, btnAge, btnReset];
+
+// btnsGroup.forEach((btn) => {
+//   btn.addEventListener("click", checker);
+// });
+
+// function checker(e) {
+//   if (e.target.hasAttribute("data-id-btn")) {
+//     for (let i = 0; i < userIds.length; i++) {
+//       tableCellId[i].textContent = userIds[i];
+//     }
+//   }
+//   if (e.target.hasAttribute("data-name-btn")) {
+//     for (let i = 0; i < userNames.length; i++) {
+//       tableCellName[i].textContent = userNames[i];
+//     }
+//   }
+//   if (e.target.hasAttribute("data-age-btn")) {
+//     for (let i = 0; i < userAges.length; i++) {
+//       tableCellAge[i].textContent = userAges[i];
+//     }
+//   }
+//   if (e.target.hasAttribute("data-reset-btn")) {
+//     const tableCellGroup = [tableCellId, tableCellName, tableCellAge];
+//     for (let i = 0; i < tableCellGroup.length; i++) {
+//       tableCellGroup[i].forEach((tablecell) => {
+//         tablecell.textContent = "";
+//       });
+//     }
+//   }
+// }
